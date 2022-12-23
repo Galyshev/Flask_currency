68400433c8c92a643ef7e159999b9c71c991d6cb
Roman <galyshev.roman@gmail.com>
2022-12-23 09:37:48 +0200

    v1.4.1 оптимизация celery beat, указан путь для хранения файлов расписания ( -s ./utils/celerybeat-schedule.db)

--- celery_work.py
+++ celery_work.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 celery = Celery('celery_work', broker='amqp://guest@localhost:5672//')
 
 
-# перед запуском воркера во втором терминале запустить "celery -A celery_work beat"
+# перед запуском воркера во втором терминале запустить "celery -A celery_work beat -s ./utils/celerybeat-schedule.db"
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(15.0, print_word.s('periodic text'), name='name_work')
